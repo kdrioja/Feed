f42077c3bd3ceb2fcc9e2d420cdf68c12f8b1933
user145766 <user145766@74-80-245-57.ic2net.net>
2018-09-19 00:38:52 -0700

    implemented add pull to refresh

--- Feed/PhotosViewController.swift
+++ Feed/PhotosViewController.swift
@@ -13,6 +13,67 @@ private let reuseIdentifier = "Cell"
 
 class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var refreshControl: UIRefreshControl!
+    var posts: [[String: Any]] = [] //Will store the posts retrieved from the network request
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(PhotosViewController.didPullToRefresh(_:)), for: .valueChanged)
+        
+        tableView.insertSubview(refreshControl, at: 0)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.rowHeight = 229
+        
+        fetchPosts()
+        
+    }
+    
+    
+    @objc func didPullToRefresh(_ refreshControl: UIRefreshControl){
+        fetchPosts()
+    }
+    
+    
+    func fetchPosts() {
+        activityIndicator.startAnimating()
+        // Network request snippet
+        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
+        
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data,
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                //print(dataDictionary)
+                
+                // TODO: Get the posts and store in posts property
+                // Get the dictionary from the response key
+                let responseDictionary = dataDictionary["response"] as! [String: Any]
+                // Store the returned array of dictionaries in our posts property
+                self.posts = responseDictionary["posts"] as! [[String: Any]]
+                // TODO: Reload the table view
+                self.tableView.reloadData()
+                self.refreshControl.endRefreshing()
+                self.activityIndicator.stopAnimating()
+            }
+        }
+        task.resume()
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.posts.count
     }
@@ -36,43 +97,4 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
             cell.photoImageView.af_setImage(withURL: url!)
         }
         return cell
-    }
-    
-    
-    @IBOutlet weak var tableView: UITableView!
-    
-    //Will store the posts retrieved from the network request
-    var posts: [[String: Any]] = []
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        tableView.delegate = self
-        tableView.dataSource = self
-        
-        // Network request snippet
-        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
-        
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        
-        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
-        
-        let task = session.dataTask(with: url) { (data, response, error) in
-            if let error = error {
-                print(error.localizedDescription)
-            } else if let data = data,
-                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                //print(dataDictionary)
-                
-                // TODO: Get the posts and store in posts property
-                // Get the dictionary from the response key
-                let responseDictionary = dataDictionary["response"] as! [String: Any]
-                // Store the returned array of dictionaries in our posts property
-                self.posts = responseDictionary["posts"] as! [[String: Any]]
-                // TODO: Reload the table view
-                self.tableView.reloadData()
-            }
-        }
-        task.resume()
-    }
-}
+    }}
